c429e1619def97c5028b5cde786962eb55242a50
Adrian Ardika Kusuma <ardika7620@gmail.com>
2022-10-12 10:31:26 -0700

    Adding Gantt Chart and Updating our readme

---
+++ untitled/firstClass.java
@@ -0,0 +1,9 @@
+public class firstClass {
+    public static void main(String[] args) {
+        firstMethod();
+    }
+    public void firstMethod(){
+        System.out.println("This is the first method");
+    }
+}
+

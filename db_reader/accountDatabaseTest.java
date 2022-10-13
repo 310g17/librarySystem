b00711bd5feeacf97f3b17406d06a987bc16d513
leminerale04 <77290006+leminerale04@users.noreply.github.com>
2022-10-13 09:33:40 -0700

    create db reader (unfinished)

---
+++ db_reader/accountDatabaseTest.java
@@ -0,0 +1,42 @@
+package cosc310;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class accountDatabaseTest {
+
+	public static void main(String[] args) {
+		String filename = "/Users/henryaugustiano/Documents/GitHub/librarySystem/Databases/userDB.csv";
+		ArrayList<accountDatabase> list = new ArrayList<>(5);
+		Path pathToFile = Paths.get(filename);
+		
+		try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) 
+		{ 
+			String line = br.readLine(); 
+			while (line != null) { 
+				String[] attributes = line.split(",");
+				Book book = createBook(attributes); 
+				books.add(book); 
+				line = br.readLine(); } 
+			} 
+		catch (IOException ioe) { 
+			ioe.printStackTrace(); 
+			}
+			}
+
+}
+
+private static accountDatabase createAccount(String[] metadata) { 
+	int uid = Integer.parseInt(metadata[0]); 
+	String uname = metadata[1];
+	String pwd = metadata[2]; 
+	int lvl = Integer.parseInt(metadata[3]);
+	return new accountDatabase(name, price, author); 
+	}
+
+}

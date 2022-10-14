4bea36e29e46d34ea6055235411c57c5220c93e0
Adrian Ardika Kusuma <ardika7620@gmail.com>
2022-10-14 11:27:27 -0700

    minimal change

--- cosc310_LibSyst/src/main.java
+++ cosc310_LibSyst/src/main.java
@@ -2,12 +2,14 @@ import java.util.List;
 import java.util.*;
 public class main  {
     public static void main(String[] args) {
+        //pull data from database class
         List<String> borrowedBooks = new ArrayList<>();
         borrowedBooks.add("Book1");
         borrowedBooks.add("book2");
         String name = "ABC";
         int uid = 123456;
         String password = "aBc223";
+        //log in will set the variables and what they can do
         Admin adminNew = new Admin(name, uid, password, borrowedBooks);
         System.out.println(adminNew.toString());
     }

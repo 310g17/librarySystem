2a8c44ce704422329d02e95ceaabde7f0968f27f
Adrian Ardika Kusuma <ardika7620@gmail.com>
2022-10-15 17:01:17 -0700

    Check

--- cosc310_LibSyst/out/production/cosc310_LibSyst/User.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.util.*;
-
-public class User extends Account_abstract{
-    public User(int UID, String name, String password, String[] borrowedBooks, LocalDate[] dateBor) {
-        super(UID, name, password, borrowedBooks, 1, dateBor);
-    }
-
-}
